09752ca29a3e08b5fa5490b82cde2270711adc28
arman-programmer <armaska2004@gmail.com>
2024-05-31 11:34:38 +0500

    changes

--- app/Http/Controllers/Admin/ProductAdminController.php
+++ app/Http/Controllers/Admin/ProductAdminController.php
@@ -200,7 +200,7 @@ class ProductAdminController extends Controller
     public function upload(Request $request)
     {
         $request->validate([
-            'file' => 'required|image|mimes:jpeg,png,jpg,gif|max:4096',
+            'file' => 'required|image|mimes:jpeg,png,jpg,webp|max:4096',
         ]);
 
         if ($request->file()) {
