cdc5346dcb878c19aaff09b4b4125af16cfd4aeb
arman-programmer <armaska2004@gmail.com>
2024-05-31 14:09:11 +0500

    changes

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function order()
+    public function orders()
     {
         $orders = Orders::all();
         return view('admin.orders', compact(
