e1a527d1070837ea7e7958b65e8cc52ebdf54e75
arman-programmer <armaska2004@gmail.com>
2024-05-31 18:24:20 +0500

    changes

--- resources/views/admin/product/reviews.blade.php
+++ resources/views/admin/product/reviews.blade.php
@@ -62,10 +62,10 @@
                                         </td>
                                         <td>
                                             {{ $review->stars ?? null }}
-                                            @for ($i = 0; $i <= $review->stars; $i++)
-                                                <span class="gl-star-rating gl-star-rating--ltr" data-star-rating="">
-                                                    <span class="gl-star-rating--stars" data-rating="5">
-                                                        <span data-index="0" data-value="1" class="gl-active">
+                                            <span class="gl-star-rating gl-star-rating--ltr" data-star-rating="">
+                                                <span class="gl-star-rating--stars" data-rating="5">
+                                                    @for ($i = 1; $i <= $review->stars; $i++)
+                                                        <span data-index="{{ $i }}" data-value="1" class="gl-active">
                                                             <svg xmlns="http://www.w3.org/2000/svg"
                                                                  class="icon gl-star-full text-primary icon-3"
                                                                  width="24"
@@ -78,12 +78,13 @@
                                                                 <path
                                                                     d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"
                                                                     stroke-width="0"
-                                                                    fill="currentColor"></path>
+                                                                    fill="currentColor">
+                                                                </path>
                                                             </svg>
                                                         </span>
-                                                    </span>
+                                                    @endfor
                                                 </span>
-                                            @endfor
+                                            </span>
                                         </td>
                                         <td>
                                             {{ $review->text ?? null }}
