e2be4663cf9332e100412f0a8459f52b4846bc86
arman-programmer <armaska2004@gmail.com>
2024-06-03 01:04:36 +0500

    changes

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -87,7 +87,7 @@
 <script src="{{ asset('assets/js/venobox.min.js') }}"></script>
 
 <!-- Main js file that contents all jQuery plugins activation. -->
-<script src="{{ asset('assets/js/main.js?v=1.8') }}"></script>
+<script src="{{ asset('assets/js/main.js?v=1.9') }}"></script>
 </body>
 
 </html>
