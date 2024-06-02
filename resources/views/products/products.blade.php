9608090b8aff2850552fbc467c8f00ab019f77b6
arman-programmer <armaska2004@gmail.com>
2024-06-02 12:55:04 +0500

    changes

--- resources/views/products/products.blade.php
+++ resources/views/products/products.blade.php
@@ -139,7 +139,7 @@
                             @endif
                             <div class="row">
                                 @foreach($products as $product)
-                                    <div class="col-6 col-md-3">
+                                    <div class="col-6 col-md-4 col-lg-3">
                                         <div class="product-grid">
                                             <div class="product-image">
                                                 <a href="{{ route('product', ['id' => $product->id]) }}" class="image">
