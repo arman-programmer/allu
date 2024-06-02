dd2e3932431df4820889527cfa29345968648e8c
arman-programmer <armaska2004@gmail.com>
2024-06-03 00:39:16 +0500

    changes

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -109,7 +109,7 @@
 <script src="{{ asset('assets/js/bootstrap.bundle.js') }}"></script>
 
 <!-- Plugins JS Files -->
-<script src="{{ asset('assets/js/toasts.js') }}"></script>
+<script src="{{ asset('assets/js/Toasts.js') }}"></script>
 <script src="{{ asset('assets/js/swiper.min.js') }}"></script>
 <script src="{{ asset('assets/js/jquery.countdown.min.js') }}"></script>
 <script src="{{ asset('assets/js/material-scrolltop.js') }}"></script>
