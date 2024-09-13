ee91a3d210a638c1a9ad1c953dbbffc5b05404e8
arman-programmer <armaska2004@gmail.com>
2024-09-13 22:08:59 +0500

    changes

--- app/Http/Controllers/Api/ApiProductController.php
+++ app/Http/Controllers/Api/ApiProductController.php
@@ -3,31 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Products\Products;
 use Illuminate\Http\Request;
 
 class ApiProductController extends Controller
 {
     public function index()
     {
-        return response()->json(Product::all());
+        return response()->json(Products::all());
     }
 
     public function store(Request $request)
     {
-        $product = Product::create($request->all());
+        $product = Products::create($request->all());
         return response()->json($product, 201);
     }
 
     public function update(Request $request, $id)
     {
-        $product = Product::findOrFail($id);
+        $product = Products::findOrFail($id);
         $product->update($request->all());
         return response()->json($product);
     }
 
     public function destroy($id)
     {
-        Product::destroy($id);
+        Products::destroy($id);
         return response()->json(null, 204);
     }
 }
