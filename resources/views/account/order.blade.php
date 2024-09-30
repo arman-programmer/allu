b400c9ffedbacdea8c30d9254c68b3a14a4e9026
arman-programmer <armaska2004@gmail.com>
2024-09-30 22:59:35 +0500

    changes

--- resources/views/account/order.blade.php
+++ resources/views/account/order.blade.php
@@ -22,17 +22,21 @@
     <div class="container">
         <div class="order-card">
             <div class="row">
-                <h5>Заказ</h5>
-                <p>чета чета</p>
-                <p>чета: <span class="font-weight-bold">24</span></p>
-            </div>
-            <div class="row">
-                <button class="btn btn--box btn--small btn--uppercase btn--blue">
-                    Назад
-                </button>
-                <button class="btn btn--box btn--small btn--uppercase btn--blue m-l-20">
-                    Назад
-                </button>
+                <div class="col-6">
+                    <h5>Заказ</h5>
+                </div>
+                <div class="col-6">
+                    <p>чета чета</p>
+                    <p>чета: <span class="font-weight-bold">24</span></p>
+                </div>
+                <div class="col-12">
+                    <button class="btn btn--box btn--small btn--uppercase btn--blue">
+                        Назад
+                    </button>
+                    <button class="btn btn--box btn--small btn--uppercase btn--blue m-l-20">
+                        Назад
+                    </button>
+                </div>
             </div>
         </div>
     </div>
